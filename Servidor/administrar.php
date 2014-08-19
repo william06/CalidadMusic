fe7b0d1df969167bd2afaa65856fd132924d6df2
Geank Valverde <geank1809@gmail.com>
2014-08-19 10:11:58 -0600

    Versión 1.3
    Modificación para subir archivos

--- Servidor/administrar.php
+++ Servidor/administrar.php
@@ -1,14 +1,16 @@
-<?php
-    echo "Hola";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title></title>
+        <title>Subir canciones</title>
     </head>
     <body>
-        
+        <form action="cargarArchivo.php" method="post" enctype="multipart/form-data">
+            <label for="archivo">Archivo:</label>
+            <input type="file" name="archivo" />
+            <br />
+
+            <input type="submit" value="Cargar Archivo" />
+        </form>
     </body>
 </html>
