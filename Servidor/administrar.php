b53e37e70d04f8926f8043324a8b87d6a16f7bb9
willliam06 <william06x@gmail.com>
2014-08-20 22:09:21 -0600

    version 1.8

--- Servidor/administrar.php
+++ Servidor/administrar.php
@@ -2,15 +2,25 @@
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title>Subir canciones</title>
+        <link href="estilos2.css" rel="stylesheet" />
+        <title>Subir archivos al servidor utilizando PHP</title>
     </head>
     <body>
+        <header class="fijo">
+           <span id="titulo">Calidad Music</span>
+        </header>
+        <br/><br/><br/>
+        <h1>Subir canciones</h1>
+        <br/>
+        <div>
         <form action="cargarArchivo.php" method="post" enctype="multipart/form-data">
             <label for="archivo">Archivo:</label>
             <input type="file" name="archivo" />
+            <br/>
             <br />
 
-            <input type="submit" value="Cargar Archivo" />
+            <input type="submit" value="Cargar Archivo" class="boton"/>
         </form>
+            </div>
     </body>
 </html>
